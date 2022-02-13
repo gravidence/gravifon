80c27629d974717bb8ee14eeddc78a4fc22e2a49
bassstorm <maksim_liauchuk@fastmail.fm>
2022-02-13 16:37:28 +0100

    Minor build tweaks

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "1.6.10" apply false
+    kotlin("plugin.serialization") version "1.6.10" apply false
+    id("org.jetbrains.compose") version "1.0.1" apply false
+}
--- gravifon/build.gradle.kts
+++ gravifon/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
-    id("org.jetbrains.compose") version "1.0.1"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.compose")
 }
 
 group = "org.gravidence"
