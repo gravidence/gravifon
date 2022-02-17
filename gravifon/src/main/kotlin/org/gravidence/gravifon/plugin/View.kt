219a8d9905194a010e79c4a660d65a1441f57fc1
bassstorm <maksim_liauchuk@fastmail.fm>
2022-02-17 21:25:55 +0100

    Small clean-up

--- gravifon/src/main/kotlin/org/gravidence/gravifon/plugin/View.kt
+++ gravifon/src/main/kotlin/org/gravidence/gravifon/plugin/View.kt
@@ -2,6 +2,9 @@ package org.gravidence.gravifon.plugin
 
 import androidx.compose.runtime.Composable
 
+/**
+ * Marker for a component that it has own composable view to render.
+ */
 interface View {
 
     @Composable
