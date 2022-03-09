d28635b8c584cc7dd4d28a94171052c9f518ed3f
bassstorm <maksim_liauchuk@fastmail.fm>
2022-03-09 22:49:19 +0100

    Complete user.getInfo API method

--- lastfm4k/src/main/kotlin/org/gravidence/lastfm4k/api/user/UserApi.kt
+++ lastfm4k/src/main/kotlin/org/gravidence/lastfm4k/api/user/UserApi.kt
@@ -4,16 +4,20 @@ import kotlinx.serialization.json.decodeFromJsonElement
 import org.gravidence.lastfm4k.api.LastfmApiContext
 import org.gravidence.lastfm4k.api.LastfmApiMethod
 import org.gravidence.lastfm4k.api.paramSessionKey
+import org.gravidence.lastfm4k.misc.Param
 import org.gravidence.lastfm4k.misc.lastfmSerializer
 import org.gravidence.lastfm4k.misc.toJsonObject
 
 class UserApi(private val context: LastfmApiContext) {
 
-    fun getInfo(): UserInfoResponse {
+    /**
+     * Fetches specific [user] info. Defaults to the authenticated user.
+     */
+    fun getInfo(user: String? = null): UserInfoResponse {
         val response = context.client.get(
             LastfmApiMethod.USER_GETINFO,
             listOf(
-                context.session?.paramSessionKey(),
+                user?.let { Param("user", it) } ?: context.session?.paramSessionKey(),
             )
         )
 
