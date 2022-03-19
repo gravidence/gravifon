6d3d4032986a709b0ff8ee4c58b1108d7141e0bb
bassstorm <maksim_liauchuk@fastmail.fm>
2022-03-19 11:05:15 +0100

    Extend VA with one more case

--- lastfm4k/src/main/kotlin/org/gravidence/lastfm4k/api/track/Track.kt
+++ lastfm4k/src/main/kotlin/org/gravidence/lastfm4k/api/track/Track.kt
@@ -16,12 +16,15 @@ data class Track(
     val duration: Long? = null
 )
 
+val VARIOUS_ARTISTS_VARIATIONS = setOf("VA", "V/A", "V.A.")
+const val VARIOUS_ARTISTS_STANDARD = "Various Artists"
+
 /**
  * Apply workarounds to make track data comply to Last.fm (sometimes controversial) limitations.
  */
 fun Track.comply(): Track {
-    return if (albumArtist == "VA" || albumArtist == "V/A") {
-        copy(albumArtist = "Various Artists")
+    return if (albumArtist in VARIOUS_ARTISTS_VARIATIONS) {
+        copy(albumArtist = VARIOUS_ARTISTS_STANDARD)
     } else {
         this
     }
