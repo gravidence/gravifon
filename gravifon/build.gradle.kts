9d8a515b647521cf644c6f0558a768366227dcb5
bassstorm <maksim_liauchuk@fastmail.fm>
2022-03-22 22:03:16 +0100

    Bump dependency versions

--- gravifon/build.gradle.kts
+++ gravifon/build.gradle.kts
@@ -23,21 +23,20 @@ dependencies {
 
     implementation(compose.desktop.currentOs)
 
-//    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
-    implementation("ch.qos.logback:logback-classic:1.2.10")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
-    implementation("org.http4k:http4k-core:4.19.1.0")
+    implementation("org.http4k:http4k-core:4.20.1.0")
     implementation("org.jsoup:jsoup:1.14.3")
     implementation("org.slf4j:jcl-over-slf4j:1.7.36")
     implementation("org.slf4j:jul-to-slf4j:1.7.36")
     implementation("org.slf4j:log4j-over-slf4j:1.7.36")
 
-    implementation("org.springframework:spring-context:5.3.15")
-    implementation("org.springframework:spring-expression:5.3.15")
+    implementation("org.springframework:spring-context:5.3.16")
+    implementation("org.springframework:spring-expression:5.3.16")
 
     implementation("net.jthink:jaudiotagger:3.0.1")
     implementation("org.freedesktop.gstreamer:gst1-java-core:1.4.0")
