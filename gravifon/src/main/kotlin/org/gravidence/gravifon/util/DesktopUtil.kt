9633f62260482b4c870ffea2c67f2955ce64f41f
bassstorm <maksim_liauchuk@fastmail.fm>
2023-08-26 23:46:26 +0200

    Improve Desktop compatibility

--- gravifon/src/main/kotlin/org/gravidence/gravifon/util/DesktopUtil.kt
+++ gravifon/src/main/kotlin/org/gravidence/gravifon/util/DesktopUtil.kt
@@ -1,5 +1,6 @@
 package org.gravidence.gravifon.util
 
+import com.sun.jna.Platform
 import mu.KotlinLogging
 import java.awt.Desktop
 import java.net.URI
@@ -10,7 +11,14 @@ object DesktopUtil {
 
     fun openInBrowser(url: String) {
         try {
-            Desktop.getDesktop().browse(URI(url))
+            // use system specific 'open' command on Linux as workaround if Gnome libraries aren't present
+            // see https://stackoverflow.com/questions/18004150/desktop-api-is-not-supported-on-the-current-platform
+            // and https://wiki.archlinux.org/title/Xdg-utils#xdg-open
+            if (Platform.isLinux()) {
+                Runtime.getRuntime().exec(arrayOf("xdg-open", url))
+            } else {
+                Desktop.getDesktop().browse(URI(url))
+            }
         } catch (e: Exception) {
             logger.warn(e) { "Failed to open URL in browser: $url" }
         }
